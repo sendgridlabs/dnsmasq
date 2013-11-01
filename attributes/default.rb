1841e8d632d630610980f5afff8845e9b7ebb32e
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-11-01 21:04:11 +0200

    fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,6 @@ default[:dnsmasq][:enable_dhcp] = false
 default[:dnsmasq][:managed_hosts] = {}
 default[:dnsmasq][:managed_hosts_bag] = "managed_hosts"
 default[:dnsmasq][:dns] = {
-  'no-poll' => nil,
-  'no-resolv' => nil,
   'server' => '127.0.0.1'
 }
 default[:dnsmasq][:dhcp] = {}
